af59fe32d54c1892173454430138d2c9af33f0fe
Sanket Naik <sanketnaik99@gmail.com>
2020-03-15 21:26:59 +0530

    DP-17 #comment Added admin Navbar.

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import * as M from "materialize-css";
 
 @Component({
-  selector: 'app-admin-dashboard',
-  templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  selector: "app-admin-dashboard",
+  templateUrl: "./admin-dashboard.component.html",
+  styleUrls: ["./admin-dashboard.component.css"]
 })
 export class AdminDashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
