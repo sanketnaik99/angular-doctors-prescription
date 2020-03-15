f6ee6d3c6981956c207a4daa73a32ba1ecf0542e
Sanket Naik <sanketnaik99@gmail.com>
2020-03-15 18:24:35 +0530

    DP-17 #start-progress Admin Login Implemented

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,38 @@
+import { AdminService } from "./admin.service";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AdminLoginComponent } from "./admin-login/admin-login.component";
+import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
+import { CanActivate } from "@angular/router";
+import { AdminAuthGuard } from "./admin.guard";
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "login"
+  },
+  {
+    path: "login",
+    component: AdminLoginComponent
+  },
+  {
+    path: "dashboard",
+    component: AdminDashboardComponent,
+    canActivate: [AdminAuthGuard]
+  }
+];
+
+@NgModule({
+  declarations: [AdminLoginComponent, AdminDashboardComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [AdminAuthGuard]
+})
+export class AdminModule {}
