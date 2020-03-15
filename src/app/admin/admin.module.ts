862df762dbcb90b19e00217ec0a37aff7e3eb6e8
Sanket Naik <sanketnaik99@gmail.com>
2020-03-15 21:26:59 +0530

    DP-17 #comment Added admin Navbar.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,6 +8,7 @@ import { AdminLoginComponent } from "./admin-login/admin-login.component";
 import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
 import { CanActivate } from "@angular/router";
 import { AdminAuthGuard } from "./admin.guard";
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
 
 const routes: Routes = [
   {
@@ -26,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AdminLoginComponent, AdminDashboardComponent],
+  declarations: [AdminLoginComponent, AdminDashboardComponent, AdminNavbarComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
