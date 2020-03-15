e99410fabd8251d18da37d440ff4ebc57a39f691
Sanket Naik <sanketnaik99@gmail.com>
2020-03-16 00:31:26 +0530

    DP-17 #resolve Admin Dashboard implemented.

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { AdminService } from "./../admin.service";
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 import * as M from "materialize-css";
 
@@ -7,7 +8,30 @@ import * as M from "materialize-css";
   styleUrls: ["./admin-dashboard.component.css"]
 })
 export class AdminDashboardComponent implements OnInit {
-  constructor() {}
+  doctors: any[];
+  patients: any[];
+  isDoctorLoading: boolean = true;
+  isPatientLoading: boolean = true;
 
-  ngOnInit() {}
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit() {
+    this.adminService.getDoctorData().then(res => {
+      this.doctors = res;
+      this.isDoctorLoading = false;
+    });
+    this.adminService.getPatientData().then(res => {
+      this.patients = res;
+      this.isPatientLoading = false;
+    });
+  }
+
+  makeAdmin(uid: string, username: string) {
+    if (
+      confirm(
+        `Are you sure you want to make ${username} an admin? \n(Grant All Permissions to this user)`
+      )
+    ) {
+    }
+  }
 }
