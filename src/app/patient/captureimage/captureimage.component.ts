33d1e9f0751a873ae89683f8331e7229b55ee1b2
belkin <sahilnair71@gmail.com>
2020-03-31 01:04:37 +0530

    zoom out done

--- src/app/patient/captureimage/captureimage.component.ts
+++ src/app/patient/captureimage/captureimage.component.ts
@@ -29,8 +29,8 @@ export class CaptureimageComponent implements OnInit {
   constraints = {
     video: {
       facingMode: "environment",
-      width: { ideal: 4096 },
-      height: { ideal: 2160 }
+      width: { ideal: 409 },
+      height: { ideal: 216 }
     }
   };
   constructor(
